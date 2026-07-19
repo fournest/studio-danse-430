f3022afaaba10da26c5857e1336689a9e0af8f00
fournest sebastien <sebfour85@gmail.com>
2026-07-19 18:32:41 +0200

    feat: mise en place du système de réinitialisation de mot de passe et migration BDD

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -73,7 +73,7 @@ class ResetPasswordController extends AbstractController
     /**
      * Validates and process the reset URL that the user clicked in their email.
      */
-    #[Route('/reset/{token}', name: 'app_reset_password')]
+    #[Route('/reset/{token}', name: 'app_reset_password', defaults: ['token' => null])]
     public function reset(Request $request, UserPasswordHasherInterface $passwordHasher, TranslatorInterface $translator, ?string $token = null): Response
     {
         if ($token) {
